d22a607797fd7bf3ae8fa1119b8a5ac85aab5a60
mazakk94 <mazakk94@gmail.com>
2016-07-31 22:57:34 +0200

    start grzebania

--- src/renderEngine/Loader.java
+++ src/renderEngine/Loader.java
@@ -103,9 +103,10 @@ public class Loader {
         vbos.add(vboID);
         GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboID);
         FloatBuffer buffer = storeDataInFloatBuffer(data);
+                            //typ vbo
         GL15.glBufferData(GL15.GL_ARRAY_BUFFER, buffer, GL15.GL_STATIC_DRAW);
         GL20.glVertexAttribPointer(attributeNumber, coordinateSize, GL11.GL_FLOAT, false, 0, 0);
-        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0); //unbind
     }
 
     private void unbindVAO() {
